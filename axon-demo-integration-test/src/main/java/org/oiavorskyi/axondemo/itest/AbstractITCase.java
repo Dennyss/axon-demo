5c9df4475dc9f8e6ba40bca40f1d10c2f5660468
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2014-12-25 17:33:04 -0500

    Refactored integration testing support to extract abstract base class

--- axon-demo-integration-test/src/main/java/org/oiavorskyi/axondemo/itest/AbstractITCase.java
+++ axon-demo-integration-test/src/main/java/org/oiavorskyi/axondemo/itest/AbstractITCase.java
@@ -1,10 +1,8 @@
 package org.oiavorskyi.axondemo.itest;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.oiavorskyi.axondemo.Application;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,24 +11,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.jms.ConnectionFactory;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertThat;
-
 
 @RunWith( SpringJUnit4ClassRunner.class )
 @ContextConfiguration(
-        classes = { Application.Config.class, SimpleITCase.StubConfiguration.class },
+        classes = { Application.Config.class, AbstractITCase.StubConfiguration.class },
         initializers = { ExecutionProfileAwareApplicationContextInitializer.class}
 )
-public class SimpleITCase {
-
-    @Autowired
-    private ConnectionFactory rawConnectionFactory;
-
-    @Test
-    public void simpleTest() throws Exception {
-        assertThat(rawConnectionFactory, instanceOf(ActiveMQConnectionFactory.class));
-    }
+public abstract class AbstractITCase {
 
     @Configuration
     @Profile( "integration" )
