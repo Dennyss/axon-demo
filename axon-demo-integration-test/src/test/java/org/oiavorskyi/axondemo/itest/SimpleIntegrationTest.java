54619eb84077c13b52ba0101a5476c97dd824288
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2014-12-24 22:45:16 -0500

    Added basic integration test template with capability of overriding particular beans in Spring context with their test implementations

---
+++ axon-demo-integration-test/src/test/java/org/oiavorskyi/axondemo/itest/SimpleIntegrationTest.java
@@ -0,0 +1,63 @@
+package org.oiavorskyi.axondemo.itest;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.oiavorskyi.axondemo.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ActiveProfilesResolver;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.jms.ConnectionFactory;
+
+import static org.hamcrest.CoreMatchers.*;
+
+
+@RunWith( SpringJUnit4ClassRunner.class )
+@ContextConfiguration( classes = { Application.Config.class,
+        SimpleIntegrationTest.StubConfiguration.class } )
+@ActiveProfiles( resolver = SimpleIntegrationTest.EnvironmentBasedProfileResolver.class )
+public class SimpleIntegrationTest {
+
+    @Autowired
+    private ConnectionFactory rawConnectionFactory;
+
+    @Test
+    public void simpleTest() throws Exception {
+        Assert.assertThat(rawConnectionFactory, instanceOf(ActiveMQConnectionFactory.class));
+    }
+
+    public static class EnvironmentBasedProfileResolver implements ActiveProfilesResolver {
+
+        @Override
+        public String[] resolve( Class<?> testClass ) {
+            String profile = "integration";
+            String executionProfile = Application.identifyCurrentExecutionProfile();
+
+            if ( !"default" .equals(executionProfile) ) {
+                // Overriding default integration profile to allow running smoke tests in
+                // production environment(s)
+                profile = executionProfile;
+            }
+            return new String[] { profile };
+        }
+    }
+
+    @Configuration
+    @Profile( "integration" )
+    public static class StubConfiguration {
+
+        @Bean
+        public ConnectionFactory rawConnectionFactory() {
+            return new ActiveMQConnectionFactory();
+        }
+
+    }
+
+}
